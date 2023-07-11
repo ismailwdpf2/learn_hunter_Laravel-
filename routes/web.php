a956117baae23a1b624b9b49c7c2781d39feb3a9
Ismail Hosen <ismailwdpf@gmail.com>
2023-07-11 22:55:24 +0600

    tessst

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TestController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    // dd(app());
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+// Route::get('test', function(){
+// return view('test');
+// });
+Route::get('test1',function(){
+return 'im ismail';
+});
+Route::view('test2', 'test');
+
+Route::get('/test3',[TestController::class, 'index']);
+
+require __DIR__.'/auth.php';
